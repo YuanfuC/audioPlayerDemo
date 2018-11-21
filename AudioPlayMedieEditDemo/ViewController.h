d49a9cc20d7ee592c3b03793ee34f706041fcf7f
ChenYuanfu <chenyuanfu@zerozero.cn>
2018-11-21 13:57:09 +0800

    Initial Commit

---
+++ AudioPlayMedieEditDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AudioPlayMedieEditDemo
+//
+//  Created by ChenYuanfu on 2018/11/21.
+//  Copyright © 2018年 Zerozero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AudioPlayMedieEditDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AudioPlayMedieEditDemo
+//
+//  Created by ChenYuanfu on 2018/11/21.
+//  Copyright © 2018年 Zerozero. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
